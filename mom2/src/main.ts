143f040ce9de7eddf7adb69d000861520503293b
Vera Kippel <vera.kippel@outlook.com>
2025-04-14 17:20:20 +0200

    Skapar klass, interface och sparar i localstorage

--- mom2/src/main.ts
+++ mom2/src/main.ts
@@ -1,24 +1,31 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface Todo {
+  task: string,
+  completed: boolean,
+  priority: number
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+class ToDoList{
+  //array med todo-objekt
+  public todos: Todo[] = [];
+  //konstruktor, ladda in från localstorage
+  constructor(){
+    this.loadFromLocalStorge();
+  }
+
+  public addTodo(task: string, priority: number): boolean{
+    if(task !== "" && priority !== 1 && priority !== 2 && priority !== 3){
+      return false;
+    }
+    return true;
+  }
+  //Spara till localstorage
+  public loadFromLocalStorge(): void{
+    const storagedTodos = localStorage.getItem("todos");
+    if(storagedTodos){
+      this.todos = JSON.parse(storagedTodos);
+    }
+  }
+
+}
