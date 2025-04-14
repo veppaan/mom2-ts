470cd9bb33001950a24f54d1ecfe32177641e4ab
Vera Kippel <vera.kippel@outlook.com>
2025-04-14 19:52:01 +0200

    Skapar funktioner i Todo-klass

--- mom2/src/main.ts
+++ mom2/src/main.ts
@@ -11,6 +11,7 @@ class ToDoList{
   public todos: Todo[] = [];
   //konstruktor, ladda in från localstorage
   constructor(){
+    this.todos = [],
     this.loadFromLocalStorge();
   }
 
@@ -18,9 +19,34 @@ class ToDoList{
     if(task !== "" && priority !== 1 && priority !== 2 && priority !== 3){
       return false;
     }
+    //Nytt objekt med todos interface
+    const newTodo: Todo = {
+      task: task,
+      priority: priority,
+      completed: false
+    }
+
+    this.todos.push(newTodo);
+    this.saveToLocalStorage();
     return true;
   }
-  //Spara till localstorage
+  //Funktion för att visa om en att-göra är avklarad
+  public markTodoCompleted(todoIndex: number): void{
+    if(todoIndex >= 0 && this.todos.length){
+      this.todos[todoIndex].completed = true;
+    }
+  }
+
+  //Hämta alla todos
+  getTodos(): Todo[]{
+    return this.todos;
+  }
+
+  //Sparar till localstorage
+  public saveToLocalStorage(): void{
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+  //Läser in från localstorage
   public loadFromLocalStorge(): void{
     const storagedTodos = localStorage.getItem("todos");
     if(storagedTodos){
